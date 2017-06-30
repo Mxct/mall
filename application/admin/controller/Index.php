8daeddb07d480a77fcb6c244c0e983da581f2846
Mxct <ct19881011@126.com>
2017-06-30 20:16:55 +0800

    tp5

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\admin\controller;
+
+class Index
+{
+	public function index()
+	{
+		return '这里是后台主页';
+	}
+}
---
+++ application/index/model/Profile.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\index\model;
+
+use think\Model;
+
+class Profile extends Model
+{
+	public function user()
+	{
+		return $this->belongsTo('User');
+	}
+
+}
