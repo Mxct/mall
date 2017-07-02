d38d1ed126a0d1d57f7c080b65eab78f8c42c48b
Mxct <ct19881011@126.com>
2017-07-02 17:17:02 +0800

    ct-0702-02-login-out

--- application/shop/controller/Index.php
+++ application/shop/controller/Index.php
@@ -11,11 +11,14 @@ class Index extends Controller
 	// 官网首页
     public function index()
     {
+        // 分配变量cookie-username
+        $this->assign('username',cookie('username'));
         return $this->fetch();
     }
     // 用户登陆
      public function login(Request $request)
     {
+        // 表单提交
         if(input('post.username')){
             // 获取post数据
             $username = input('post.username');
@@ -23,9 +26,9 @@ class Index extends Controller
             $yzm = input('post.yzm');
             // 查询user表是否存在对应id
             $user = new User;
-            $result = $user->where('username',$username)->value('id');
+            $idResult = $user->where('username',$username)->value('id');
             // 判断用户名是否正确
-            if($result){
+            if($idResult){
                 echo '用户名正确';
             }else {
                 echo '用户名错误';
@@ -40,16 +43,35 @@ class Index extends Controller
                 echo '密码错误';
                 exit;
             }
+            // 查询用户类型type
+            $typeResult = $user->where('username',$username)->value('type');
             // 验证验证码是否正确
             if(!captcha_check($yzm)){
             echo '验证失败';
             $this->error('登录验证失败');
             } else {
                 // 设置cookie
-                
+                cookie('username',$username,84600);
+                cookie('uid',$idResult,84600);
+                cookie('usertype', $typeResult,84600);
                 $this->success('登录成功','shop/index/index');
             }
         }
        return $this->fetch();
+    }
+    // 用户退出，清楚cookie
+    public function logout()
+    {
+        // 清空cookie
+        cookie('username',null);
+        cookie('uid',null);
+        cookie('usertype',null);
+        // 跳转回到商城首页
+        // $url = url('/shop/index/index');
+        $this->success('退出成功','shop/index/index');
+    }
+    public function reg()
+    {
+        return $this->fetch();
     }
 }
