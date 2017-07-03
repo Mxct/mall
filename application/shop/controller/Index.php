087a9c85286ea68646cbe3124f0d261bdee1dc02
Mxct <ct19881011@126.com>
2017-07-03 10:04:47 +0800

    ct-070301-login/reg-ajax-over

--- application/shop/controller/Index.php
+++ application/shop/controller/Index.php
@@ -15,7 +15,58 @@ class Index extends Controller
         $this->assign('username',cookie('username'));
         return $this->fetch();
     }
-    // 用户登陆
+    // 登录Ajax验证
+    public function nameAj()
+    {
+        // 接收Ajax数据
+        // 用户名验证
+
+        if($_GET['username']){
+            // 查询数据库
+            $username = $_GET['username'];
+            $user = new User;
+            $idResult = $user->where('username',$username)->value('id');
+            // 判断用户名是否正确
+            if($idResult){
+                echo $username.'用户名正确';
+            }else {
+                echo '用户名有误，请重新输入!';
+            }
+        }
+
+    }
+    public function pwdAj()
+    {
+        // 接收Ajax数据
+        // 密码验证
+        if($_GET['password']){
+            $username = $_GET['username'];
+            $pwd = md5(trim($_GET['password']));
+            // 查询数据库
+            $user = new User;
+            $pwdResult = $user->where('username',$username)->value('password');
+            // 判断用户名是否正确
+            if($pwdResult == $pwd){
+                echo '密码正确';
+            }else {
+                echo '密码有误，请重新输入!';
+            }
+        }
+    }
+    public function yzmAj()
+    {
+        // 接收Ajax数据
+        // 验证码验证,备注只能验证一次；所以提交表单信息会发生验证错误
+        if($_GET['yzm']){
+            $yzm = $_GET['yzm'];
+            if(!captcha_check($yzm)){
+                echo '验证码有误,重新输入!';
+            }else {
+                echo '验证码正确!';
+            }
+        }
+    }
+    // 用户提交登陆
      public function login(Request $request)
     {
         // 表单提交
@@ -47,8 +98,8 @@ class Index extends Controller
             $typeResult = $user->where('username',$username)->value('type');
             // 验证验证码是否正确
             if(!captcha_check($yzm)){
-            echo '验证失败';
-            $this->error('登录验证失败');
+                echo '验证失败'.$yzm;
+                $this->error('登录验证失败');
             } else {
                 // 设置cookie
                 cookie('username',$username,84600);
@@ -70,8 +121,73 @@ class Index extends Controller
         // $url = url('/shop/index/index');
         $this->success('退出成功','shop/index/index');
     }
+    // 用户注册
     public function reg()
     {
         return $this->fetch();
+    }
+     public function regnameAj()
+    {
+        // 接收regAjax数据
+        // 用户名验证
+
+        if($_GET['username']){
+            // 查询数据库
+            $username = $_GET['username'];
+            // 用户名长度
+            $len = strlen($username);
+            $user = new User;
+            $idResult = $user->where('username',$username)->value('id');
+            // 判断用户名长度
+            if($len >= 6) {
+                // 判断用户名是否存在
+                if($idResult){
+                    echo '该用户名已存在';
+                }else {
+                    echo '该用户名可以使用!';
+                }
+            } else {
+                echo '用户名长度至少6个字符';
+            }
+
+        }
+    }
+    // 注册密码检验
+    public function regpwdAj()
+    {
+        // 接收Ajax数据
+        // 密码类型验证，需包含字母，长度6以上
+        if($_GET['password']){
+            $pwd = $_GET['password'];
+            // 获取长度
+            $len = strlen($pwd);
+            // 判断类型
+            $reg='/[a-zA-Z]/';
+            if($len >= 6) {
+                if (!preg_match($reg,$pwd,$match)){
+                    echo '密码不能纯数字，需包含字母';
+                } else {
+                    echo '该密码可以使用';
+                }
+            } else {
+                echo '密码长度至少6个字符';
+            }
+        }
+    }
+     public function regemailAj()
+    {
+        // 接收Ajax数据
+        // 邮箱类型验证，需包含字母，长度6以上
+        if($_GET['email']){
+            $email = $_GET['email'];
+            // 判断类型
+            $reg = '/\w+@(\w+\.)+(com|cn|net)/';
+
+            if (!preg_match($reg,$email,$match)){
+                echo '邮箱格式不对，请重新输入';
+            } else {
+                echo '该邮箱可以使用';
+            }
+        }
     }
 }
